2890e81467b5896434fe1b7e62955f1e00e7d585
nhenz9 <120919676+nhenz9@users.noreply.github.com>
2023-01-19 14:18:46 +0700

    belajar github

---
+++ pohon.py
@@ -0,0 +1,36 @@
+import turtle
+
+li = turtle.Turtle()
+li.screen.bgcolor("black")
+li.pensize(10)
+li.color("red")
+li.left(90)
+li.backward(100)
+li.speed(1000)
+li.shape("turtle")
+
+def tree(i):
+    if i<10:
+        return
+    else:
+        li.forward(i)
+        li.color("orange")
+        li.circle(2)
+        li.color("red")
+        li.left(30)
+
+        tree(3*i/4)
+
+        li.right(60)
+
+        tree(3*i/4)
+
+        li.left(30)
+        li.backward(i)
+
+tree(100)
+turtle.done
+
+
+
+        
